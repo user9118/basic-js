aced28b9388071e7409ac2d3c4f52aab1b4634aa
Андрей М <a.moskovkinq@gmail.com>
2022-10-17 16:47:46 +0300

    feat: delete-digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,10 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+ function deleteDigit(n) {
+  let newN = String(n);
+  newN = newN.split('');
+     if (newN[0]<newN[1]){
+         newN.splice(0, 1);
+     } else {
+         let smallIndex
+         for (let i = 1; i < newN.length; i++) {
+             if (newN[i]< newN[i-1]){
+                 smallIndex = i;
+             }
+         }
+         newN.splice(smallIndex, 1);
+     }
+
+     newN = Number(newN.join(''));
+     return newN
+ }
 
 module.exports = {
   deleteDigit
--- test.js
+++ test.js
@@ -1,46 +1,25 @@
-function repeater(str, options) {
-    str = String(str);
-    let newStr = '';
-    let repeatN = options['repeatTimes'];
-    if (repeatN == undefined) {
-        repeatN = 1;
-    }
-    console.log(repeatN)
-    let separatorN = options['separator'];
-    if (separatorN == 'undefined') {
-        separatorN = '+';
-    }
-    console.log(separatorN)
-    let additionN = String(options['addition']);
-    if (additionN == 'undefined') {
-        additionN = '';
-    }
-    console.log(additionN)
-    let additionRepeatTimesN = options['additionRepeatTimes'];
-    if (additionRepeatTimesN == undefined) {
-        additionRepeatTimesN = 1;
-    }
-    console.log(additionRepeatTimesN)
-    let additionSeparatorN = String(options['additionSeparator']);
-    if (additionSeparatorN == 'undefined') {
-        additionSeparatorN = '|';
-    }
-    console.log(additionSeparatorN)
 
-    for (let i = 0; i < repeatN; i++) {
-        let additionStr = '';
-        if (additionRepeatTimesN > 0) {
-            for (let j = 0; j < additionRepeatTimesN; j++) {
-                additionStr = additionStr + additionN + additionSeparatorN;
-            }
-            additionStr = additionStr.slice(0, - additionSeparatorN.length);
-        } else {
-            additionStr = '';
-        }
-        newStr = newStr + str + additionStr + separatorN;
-    }
-    newStr = newStr.slice(0, - separatorN.length)
-    console.log(newStr);
-}
 
-repeater('TESTstr', { separator: 'ds', addition: 'ADD!', additionSeparator: ')))000' });
+ function deleteDigit(n) {
+    let newN = String(n);
+    newN = newN.split('');
+    console.log(newN)
+       if (newN[0]<newN[1]){
+           newN.splice(0, 1);
+       } else {
+           let smallIndex
+           for (let i = 1; i < newN.length; i++) {
+               if (newN[i]< newN[i-1]){
+                   smallIndex = i;
+               }
+           }
+           newN.splice(smallIndex, 1);
+       }
+  
+       newN = newN.join('')
+       console.log(newN)
+       return newN
+  
+   }
+
+deleteDigit(864721)
